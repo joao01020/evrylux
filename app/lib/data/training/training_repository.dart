28ab843e0c36c5b86d787134e87cedddf07a87f2
joao01020 <joaosaless26@gmail.com>
2026-07-25 11:38:24 -0300

    refactor(training): update training repository data structure

--- app/lib/data/training/training_repository.dart
+++ app/lib/data/training/training_repository.dart
@@ -1,5 +1,24 @@
 import '../../core/storage/storage_service.dart';
 
+/*
+
+|
+| Screen
+|    ↓
+| Controller
+|    ↓
+| Service
+|    ↓
+| Repository
+|    ↓
+| StorageService
+|
+| Cada camada recebe apenas a dependência da camada abaixo.
+| Assim toda a aplicação utiliza uma única instância dos serviços,
+| facilitando manutenção, testes e evolução do projeto.
+
+*/
+
 class TrainingRepository {
   Future<
     Map<
